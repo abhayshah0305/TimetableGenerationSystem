21856c03de4f4c9dff5392cac09d22b6d08ecb73
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2021-12-19 15:32:33 +0530

    Update settings.py

--- projttgs/projttgs/settings.py
+++ projttgs/projttgs/settings.py
@@ -132,7 +132,7 @@ LOGIN_REDIRECT_URL = 'admindash'
 LOGOUT_URL = 'logout'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'codevoid12@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
