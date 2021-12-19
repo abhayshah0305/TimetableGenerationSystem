8d07fb3b23a2d9ab16b4b4148ef9cad4d1b7202f
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2021-12-19 15:32:12 +0530

    Update settings.py

--- projttgs/projttgs/settings.py
+++ projttgs/projttgs/settings.py
@@ -133,7 +133,7 @@ LOGOUT_URL = 'logout'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'codevoid12@gmail.com'
-EMAIL_HOST_PASSWORD = 'anyonecancode'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
